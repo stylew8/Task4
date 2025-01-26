35b316840fde1ad68f2759bbcd228055ac967e95
stylew8 <vaniasavochkin@gmail.com>
2025-01-26 14:31:18 +0200

    react url fixed, fixed dockerfile net

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://localhost:7257/',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://uniqum.school/api/',
   headers: {
     'Content-Type': 'application/json',
   },
